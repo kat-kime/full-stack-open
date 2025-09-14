1311a3d2847ade8f50158254f962331612b18980
Katasha Kime <katckime@gmail.com>
2025-09-13 18:03:43 -0700

    end of reading

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,62 +1,37 @@
 import { useState } from "react"
 
-const Display = ({ counter }) => <div>{counter}</div>
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-const History = (props) => {
-  console.log('props.allClicks.length', props.allClicks.length)
-
-  if (props.allClicks.length === 0) {
-    return (
-      <div>the app works by pressing the buttons</div>
-    )
-  }
-
+const Button = (props) => {
   return (
-      <div>{props.allClicks.join('')}</div>
+    <div>
+      <button onClick={props.onClick}>
+       {props.text} 
+      </button>
+    </div>
   )
 }
 
 const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [value, setValue] = useState(10)
 
-  const [allClicks, setAllClicks] = useState([])
-  const [total, setTotal] = useState(0)
-
-  const incLeft = () => {
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setAllClicks(allClicks.concat('L'))
-    console.log('left before:', left)
-    console.log('right before:', right)
-    setTotal(updatedLeft + right)
+  const handleOnClick = () => {
+    setValue(0)
   }
-
-  const incRight = () => {
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setAllClicks(allClicks.concat('R'))
-    console.log('left before:', left)
-    console.log('right before:', right)
-    setTotal(updatedRight + left)
-  }
-
-  console.log("all clicks:", allClicks)
 
   return (
     <div>
-      <Display counter={left} />
-      <Button 
-        onClick={incLeft}
-        text='plus left' 
-      />
-      <Display counter={right} />
-      <Button
-        onClick={incRight}
-        text='plus right'
-      />
-      <History allClicks={allClicks} />
-      <Display counter={total} />
+      {value}
+    <Button 
+      onClick={handleOnClick} 
+      text={"reset this button to zero"}
+    />
+    <Button 
+      onClick={() => setValue(value + 1)} 
+      text={"increment"}
+    />
+    <Button 
+      onClick={() => setValue(value - 1)} 
+      text={"decrement"}
+    />
     </div>
   )
 }
