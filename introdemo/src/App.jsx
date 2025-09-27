dbb035eda015cb93864d3e8ddf4835593c817ec6
Katasha Kime <katckime@gmail.com>
2025-09-27 14:16:54 -0700

    anecdote step 2

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -28,35 +28,87 @@ const Statistics = (props) => {
   return (
     <div>
       <table>
-        <tr>
-          <td>good</td>
-          <td><StatisticsLine text={props.good} />
-          </td>
-        </tr>
-        <tr>
-          <td>bad</td>
-          <td><StatisticsLine text={props.bad} /></td>
-        </tr>
-        <tr>
-          <td>neutral</td>
-          <td><StatisticsLine text={props.neutral} /></td>
-        </tr>
-        <tr>
-          <td>average</td>
-          <td><StatisticsLine text={props.score/props.total} /></td>
-        </tr>
-        <tr>
-          <td>total</td>
-          <td><StatisticsLine text={props.total} /></td>
-        </tr>
-        <tr>
-          <td>percent good</td>
-          <td><StatisticsLine text={((props.good / props.total) * 100) + "%"} /></td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td><StatisticsLine text={props.good} />
+            </td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td><StatisticsLine text={props.bad} /></td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td><StatisticsLine text={props.neutral} /></td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td><StatisticsLine text={props.score/props.total} /></td>
+          </tr>
+          <tr>
+            <td>total</td>
+            <td><StatisticsLine text={props.total} /></td>
+          </tr>
+          <tr>
+            <td>percent good</td>
+            <td><StatisticsLine text={((props.good / props.total) * 100) + "%"} /></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
 
+}
+
+const RandomAnecdote = (props) => {
+  const [anecdoteIndex, setAnecdoteIndex] = useState(0)
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0
+  })
+
+  const anecdotes = [
+      'If it hurts, do it more often.',
+      'Adding manpower to a late software project makes it later!',
+      'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      'Premature optimization is the root of all evil.',
+      'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+      'The only way to go fast, is to go well.'
+  ]
+
+  const handleSelectAnecdote = () => {
+    console.log("anecdotes length:", anecdotes.length)
+    const updatedIndex = Math.floor(Math.random() * anecdotes.length)
+    console.log("updated index:", updatedIndex)
+    setAnecdoteIndex(updatedIndex) 
+  }
+
+  const handleAnecdoteVote = () => {
+    const copy = { ...votes }
+    copy[anecdoteIndex] += 1
+
+    setVotes(copy)
+  }
+
+  return (
+    <div>
+      <Button onClick={handleAnecdoteVote} text={"vote"} />
+      <Button onClick={handleSelectAnecdote} text={"next anecdote"}/>
+      <br />
+      {anecdotes[anecdoteIndex]}
+      <br />
+      {"has " + votes[anecdoteIndex] + " votes"}
+    </div>
+  )
 }
 
 const App = () => {
@@ -65,7 +117,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [total, setTotal] = useState(0)
   const [score, setScore] = useState(0)
-
+  
+  
   const handleVoteGood = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
@@ -105,6 +158,8 @@ const App = () => {
         total={total}
         score={score}
       />
+      <br />
+      <RandomAnecdote />
     </div>
   )
 }
