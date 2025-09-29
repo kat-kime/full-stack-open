758ec18ab1b41d5e3e1937123077380bdb60ef42
Katasha Kime <katckime@gmail.com>
2025-09-28 18:11:30 -0700

    ch2 exercise, step 7

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,43 +1,69 @@
-const Header = (props) => {
+const Header = ({header}) => {
   return (
     <>
-    <h1>{props.course}</h1>
+    <h1>{header}</h1>
     </>
   )
 }
 
-const Section = (props) => {
+const Section = ({name, exercises}) => {
   return (
     <>
-    <p>{props.part} {props.exercises}</p>
+    <p>{name} {exercises}</p>
     </>
   )
 }
 
-const Summary = (props) => {
+const Summary = ({courses}) => {
   return (
     <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>
+      Number of exercises {courses.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.exercises, 0
+      )}
+    </p>
     </>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header header={course.name}/>
+      {course.parts.map(c => 
+        <Section key={c.id} name={c.name} exercises={c.exercises} />
+      )}
+      <Summary courses={course.parts}/>
+    </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Section part={part1} exercises={exercises1} />
-      <Section part={part2} exercises={exercises2} />
-      <Section part={part3} exercises={exercises3} />
-      <Summary exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Course course={course} />
     </>
   )
 }
