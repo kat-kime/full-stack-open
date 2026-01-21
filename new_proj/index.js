05be4fddbeff43b81d793bb19a273a846fc4cf44
Katasha Kime <katckime@gmail.com>
2026-01-21 08:21:36 -0800

    integrating db into routes

--- new_proj/index.js
+++ new_proj/index.js
@@ -1,38 +1,13 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-
 const Note = require('./models/note')
 
 app.use(express.json())
 app.use(express.static('dist'))
 
 const PORT = process.env.PORT || 3001
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-const generateId = () => {
-  const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-
-  return String(maxId + 1)
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello, world</h1>')
@@ -46,12 +21,7 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  return Note.findById(id).then(note => response.json(note))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -62,7 +32,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  console.log('received note:', body)
 
   if (!body.content) {
     return response.status(400).json({
@@ -70,14 +39,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  return note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.put('/api/notes/:id', (request, response) => {
