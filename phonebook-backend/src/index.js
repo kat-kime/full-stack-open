ec43f3f960cf946b34ef8c94d546fc8137c7cb81
Katasha Kime <katckime@gmail.com>
2026-01-25 07:30:02 -0800

    uibuild

--- phonebook-backend/src/index.js
+++ phonebook-backend/src/index.js
@@ -9,6 +9,18 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3001
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  }
+
+  next(error)
+}
+
 morgan.token('body', (request, response) => {
   if (request.body) {
     const body = request.body
@@ -19,11 +31,11 @@ morgan.token('body', (request, response) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-app.delete('/api/contacts/:id', (request, response) => {
+app.delete('/api/contacts/:id', (request, response, next) => {
   const id = request.params.id
-  Contact.deleteOne({_id: id})
+  Contact.findByIdAndDelete(id)
     .then(result => response.status(204).end())
-    .catch(error => console.log('Error deleting contact', error))
+    .catch(error => next(error))
 })
 
 app.get('/', (request, response) => {
@@ -41,30 +53,59 @@ app.get('/info', (request, response) => {
     .then(contacts => response.send(`<p>Phonebook has info for ${contacts.length} people<p><p>${new Date(Date.now()).toString()}<p>`))
 })
 
-app.get('/api/contacts/:id', (request, response) => {
+app.get('/api/contacts/:id', (request, response, next) => {
   const id = request.params.id
-  return Contact.findById(id).then(contact => response.json(contact))
-})
+  return Contact.findById(id)
+    .then(contact => {
+      if (contact) {
+        response.json(contact)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  }
+)
 
 app.post('/api/contacts', (request, response) => {
   morgan('tiny')
-
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number missing'
+  const { name, number } = request.body
+  Contact.find({ name: name })
+    .then(contact => {
+      if (contact) {
+        console.log('returned contact:', contact)
+        contact.number = number
+        return contact.save().then(updatedContact => response.json(updatedContact))
+      }
     })
-  }
 
   const contact = new Contact({
-    name: body.name,
-    number: body.number
+    name: name,
+    number: number
   })
 
   return contact.save()
     .then(savedContact => response.json(savedContact))
 })
+
+app.put('/api/contacts/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Contact.findById(request.params.id)
+    .then(contact => {
+      if (!contact) {
+        return response.status(404).end()
+      }
+
+      contact.name = name
+      contact.number = number
+
+      return contact.save().then(savedContact => response.json(savedContact))
+    })
+    .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
